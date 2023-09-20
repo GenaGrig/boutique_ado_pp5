6e6db57c69b3185d91fe8662e06270f82a435855
GenaGrig <gennadiuss@gmail.com>
2023-09-20 18:11:14 +0000

    Added products app, models and fixtures

---
+++ products/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+        
+    def get_friendly_name(self):
+        return self.friendly_name
+    
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True) # SKU = Stock Keeping Unit
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True) # BooleanField is a checkbox
+    price = models.DecimalField(max_digits=6, decimal_places=2) # max_digits is the maximum number of digits allowed in the number, decimal_places is the number of decimal places allowed
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True) # max_digits is the maximum number of digits allowed in the number, decimal_places is the number of decimal places allowed
+    image_url = models.URLField(max_length=1024, null=True, blank=True) # URLField is a text field for a URL
+    image = models.ImageField(null=True, blank=True) # ImageField is for an image
+    
+    def __str__(self):
+        return self.name
+    
+
+
+
+
