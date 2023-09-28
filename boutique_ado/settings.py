d68e014158e6641a85a2a42c7eebf27e52c31a99
GenaGrig <gennadiuss@gmail.com>
2023-09-28 15:59:16 +0000

    Added toasts

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -91,6 +91,8 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.template.context_processors.request",
     )
 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
