d5eb8cd72b18e6df50f2dbb96c58b4801072ee8c
GenaGrig <gennadiuss@gmail.com>
2023-10-05 19:22:49 +0000

    Completed webhook handler

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -209,4 +209,3 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
-
