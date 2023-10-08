629fdbc842e4c1edf1f30ccb9c970b0a9b8cdc16
GenaGrig <gennadiuss@gmail.com>
2023-10-08 16:43:08 +0000

    Hiding secret key and changing debug

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&_+yi(&26%8=8r#m4=^_-$a0%%()wer@p31guzi$)lh8^s(p#3'
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-genagrig-boutiqueadopp5-nowvua2sjeb.ws-eu105.gitpod.io',
