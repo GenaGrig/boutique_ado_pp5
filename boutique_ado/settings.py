eae6346f3c4e3c86b7dbd8f3c459deb69b66c58f
GenaGrig <gennadiuss@gmail.com>
2023-10-08 21:36:21 +0000

    Settings.py change in databases

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -158,10 +158,17 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
-
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
